bb610e7c0ce67a7f38cb7520f991d6083eeab794
Ruben Gil <gil.r419@gmail.com>
2017-07-31 11:01:17 -0400

    Add function to create a dictionary with relevant bus data

--- bus_data.py
+++ bus_data.py
@@ -1,36 +1,45 @@
 import requests, json
 
 def get_bus_data(bus):
-    url = 'http://bustime.mta.info/api/search?q=' + bus
+    # Takes a dictionary containing bus data from the matches list or the
+    # the suggestions list. Both the matches and suggestions lists can be found
+    # in the searchResults in the response from the api request below.
+    # Returns a compacted version of the bus data. (Compacted version contains
+    # destinations, directionIds, Id, and shortName)
+    def compact_bus_data(data):
+        bus_data = {
+            'empty': False
+        }
 
-    response = requests.get(url)
-
-    data = json.loads(response.text)
-
-    is_empty = data["searchResults"]["empty"]
-    matches = data["searchResults"]["matches"]
-    suggestions = data["searchResults"]["suggestions"]
-
-    bus_data = {
-        "empty": False
-    }
-
-    #
-    if len(matches):
-        directions = matches[0]["directions"]
+        directions = data['directions']
 
         direction0 = {
-            "destination": str(directions[0]["destination"]),
-            "directionId": int(directions[0]["directionId"])
+            'destination': str(directions[0]['destination']),
+            'directionId': int(directions[0]['directionId'])
         }
         direction1 = {
-            "destination": str(directions[1]["destination"]),
-            "directionId": int(directions[1]["directionId"])
+            'destination': str(directions[1]['destination']),
+            'directionId': int(directions[1]['directionId'])
         }
 
-        bus_data["directions"] = [direction0, direction1]
-        bus_data["routeId"] = str(matches[0]["id"])
-        bus_data["bus"] = str(matches[0]["shortName"])
+        bus_data['directions'] = [direction0, direction1]
+        bus_data['routeId'] = str(data['id'])
+        bus_data['bus'] = str(data['shortName'])
+
+        return bus_data
 
 
-    return bus_data
+
+
+    url = 'http://bustime.mta.info/api/search?q=' + bus
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    is_empty = data['searchResults']['empty']
+    matches = data['searchResults']['matches']
+    suggestions = data['searchResults']['suggestions']
+
+    # If there is data for the bus passed as argument, take what is necessary
+    # from the data and return it.
+    if len(matches):
+        return compact_bus_data(matches[0])
