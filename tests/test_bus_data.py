b632df161e750ee3a19b8cbc61fc7aaea8d52ea7
Ruben Gil <gil.r419@gmail.com>
2017-07-31 12:58:18 -0400

    Add test for when get_bus_data has no matching data or suggestions

--- tests/test_bus_data.py
+++ tests/test_bus_data.py
@@ -25,7 +25,7 @@ def test_get_bus_data_suggestions():
     assert type(bus_data['suggestions']) == list
     assert len(bus_data['suggestions']) > 0
 
-    # The first element in the suggested data list
+    # The first element in the suggested data list.
     suggested_data = bus_data['suggestions'][0]
 
     assert type(suggested_data["directions"]) == list
@@ -38,3 +38,14 @@ def test_get_bus_data_suggestions():
     assert type(suggested_data["directions"][0]["directionId"]) == int
     assert type(suggested_data["routeId"]) == str
     assert type(suggested_data["bus"]) == str
+
+
+# Test get_bus_data function with a invalid bus as argument.
+# No matching bus data or suggestions will be found.
+# Returns a dictionary with key 'empty' and value True.
+def test_get_bus_data_no_mathches_no_suggestions():
+    bus_data = get_bus_data('z30089')
+
+    assert len(bus_data.keys()) == 1
+    assert bus_data['empty'] == True
+    assert type(bus_data['empty']) == bool
