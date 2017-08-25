1adc9e51eea28817748db2974f80437c6fd0a8f5
Ruben Gil <gil.r419@gmail.com>
2017-08-25 17:18:57 -0400

    Add tests for timestamp property of Stop class

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -13,4 +13,14 @@ class TestStopId():
         new_stop = Stop('MTA_501248')
         new_stop.stop_id = 'MTA_550943'
 
-        assert new_stop.stop_id == 'MTA_501248'   
+        assert new_stop.stop_id == 'MTA_501248'
+
+
+class TestTimestamp():
+    # Test a changing timestamp.
+    def test_stop_id(self):
+        new_stop = Stop('MTA_501248')
+        savedTimestamp = new_stop.timestamp
+        new_stop.timestamp = '131312344'
+
+        assert new_stop.timestamp == savedTimestamp
