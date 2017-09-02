06a9f5dac7cea58ff313555e1f72f5fd60a28464
Ruben Gil <gil.r419@gmail.com>
2017-09-02 14:44:27 -0400

    Create Stop class

--- mtabusdata/entities.py
+++ mtabusdata/entities.py
@@ -1,4 +1,6 @@
-from _data_fetch import get_bus_data, get_bus_stops
+import copy
+
+from _data_fetch import get_bus_data, get_bus_stops, get_stop_data
 
 class Bus(object):
     def __init__(self, name = '', directionId = 0):
@@ -91,3 +93,64 @@ class Bus(object):
         }
 
         return self.stops
+
+
+class Stop(object):
+    def __init__(self, stop_id):
+        data = get_stop_data(stop_id)
+
+        self._stop_id = stop_id.upper()
+        self._timestamp = data['timestamp']
+        self._next_buses = data['next_buses']
+        self._stop_info = data['stop']
+        self._situations = data['situations']
+
+
+    # Getter and Setters for stop_id property
+    @property
+    def stop_id(self):
+        return self._stop_id
+
+    @stop_id.setter
+    def stop_id(self, stop_id):
+        pass
+
+
+    # Getter and Setter for timestamp property
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @timestamp.setter
+    def timestamp(self, timestamp):
+        pass
+
+
+    # Getter and Setter for next_buses property
+    @property
+    def next_buses(self):
+        return copy.deepcopy(self._next_buses)
+
+    @next_buses.setter
+    def next_buses(self, next_buses):
+        pass
+
+
+    # Getter and Setter for stop_info property
+    @property
+    def stop_info(self):
+        return copy.deepcopy(self._stop_info)
+
+    @stop_info.setter
+    def stop_info(self, stop_info):
+        pass
+
+
+    # Getter and Setter for situations property
+    @property
+    def situations(self):
+        return copy.deepcopy(self._situations)
+
+    @situations.setter
+    def situatuations(self, situations):
+        pass
